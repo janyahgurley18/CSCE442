f0f88f14d8c6f6e8f14fbd526aff37675c48eb2b
janyahgurley18 <jzgurley@email.sc.edu>
2024-12-06 19:54:52 -0500

    Add files via upload

---
+++ Bread.java
@@ -0,0 +1,24 @@
+//Copyrights JaNyah Gurley
+public class Lab1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Drawing the intial shape:");
+		System.out.println(" _   _");
+		System.out.println("( `^`))");
+		System.out.println("|    ||");
+		System.out.println("|    ||");
+		System.out.println("'- - -'");
+	
+		
+		System.out.println("Displaying 2 copies of the shape that are horizontally adjacent: ");
+		
+		System.out.println(" _    _"+"\t"+"         _    _");
+		System.out.println("( `^` ))"+"\t"+"( `^` ))");
+		System.out.println("|     ||"+"\t"+"|     ||");
+		System.out.println("|     ||"+"\t"+"|     ||");
+		System.out.println("'- - - '"+"\t"+"'- - - '");
+	}
+
+}
